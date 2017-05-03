b6dd6f44ec179f35e5dfc876bcd6d8720d688696
Rosanne Lai <rosannelai@gmail.com>
2017-05-02 23:05:35 -0400

    Add files via upload

---
+++ face_landmark_detection.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image and
+#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
+#   points on the face such as the corners of the mouth, along the eyebrows, on
+#   the eyes, and so forth.
+#
+#   This face detector is made using the classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
+#   and sliding window detection scheme.  The pose estimator was created by
+#   using dlib's implementation of the paper:
+#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
+#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
+#   and was trained on the iBUG 300-W face landmark dataset.
+#
+#   Also, note that you can train your own models using dlib's machine learning
+#   tools. See train_shape_predictor.py to see an example.
+#
+#   You can get the shape_predictor_68_face_landmarks.dat file from:
+#   http://sourceforge.net/projects/dclib/files/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#   or
+#       python setup.py install --yes USE_AVX_INSTRUCTIONS
+#   if you have a CPU that supports AVX instructions, since this makes some
+#   things run faster.  
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake and boost-python installed.  On Ubuntu, this can be done easily by
+#   running the command:
+#       sudo apt-get install libboost-python-dev cmake
+#
+#   Also note that this example requires scikit-image which can be installed
+#   via the command:
+#       pip install scikit-image
+#   Or downloaded from http://scikit-image.org/download.html. 
+
+import sys
+import os
+import dlib
+import glob2
+from skimage import io
+import numpy as np
+
+predictor_path = "shape_predictor_68_face_landmarks.dat"
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+win = dlib.image_window()
+
+for f in glob2.glob("*.jpg"):
+    print("Processing file: {}".format(f))
+    img = io.imread(f)
+
+    win.clear_overlay()
+    win.set_image(img)
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    dets = detector(img, 1)
+    print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+                                                  shape.part(1)))
+        # Draw the face landmarks on the screen.
+        win.add_overlay(shape)
+
+        # Write landmarks to txt
+        vec = np.empty([80, 2], dtype=int)
+        for b in range(68):
+            vec[b][0] = shape.part(b).x
+            vec[b][1] = shape.part(b).y
+        vec[68][0]=495
+        vec[68][1]=400
+        vec[69][0]=264
+        vec[69][1]=736
+        vec[70][0]=1
+        vec[70][1]=680
+        vec[71][0]=599
+        vec[71][1]=588
+        vec[72][0]=1
+        vec[72][1]=1
+        vec[73][0]=1
+        vec[73][1]=400
+        vec[74][0]=1
+        vec[74][1]=799
+        vec[75][0]=300
+        vec[75][1]=799
+        vec[76][0]=599
+        vec[76][1]=799
+        vec[77][0]=599
+        vec[77][1]=400
+        vec[78][0]=599
+        vec[78][1]=1
+        vec[79][0]=300
+        vec[79][1]=1
+        
+        
+        np.savetxt(f+".txt",vec, fmt='%.0d')
+
+    win.add_overlay(dets)
+
+dlib.hit_enter_to_continue()
